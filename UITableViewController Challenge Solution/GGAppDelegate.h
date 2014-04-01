001053b1ff3953a063942a6f201dcde0fa8328e9
gelferstone <ideainnovation@hotmail.com>
2014-04-01 15:17:34 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/GGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GGAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Pisit Luanprawat on 4/1/14.
+//  Copyright (c) 2014 Idea Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/GGViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Pisit Luanprawat on 4/1/14.
+//  Copyright (c) 2014 Idea Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGViewController : UIViewController
+
+@end
